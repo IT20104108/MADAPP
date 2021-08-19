004756b10fd5ec7aee2a043f63d9d835916f0cd2
Aroshini88 <arodeez88@gmail.com>
2021-08-19 19:43:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/quicklearn/addquestion.java
@@ -0,0 +1,14 @@
+package com.example.quicklearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addquestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addquestion);
+    }
+}
---
+++ app/src/main/java/com/example/quicklearn/miniquiz.java
@@ -0,0 +1,14 @@
+package com.example.quicklearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class miniquiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miniquiz);
+    }
+}
